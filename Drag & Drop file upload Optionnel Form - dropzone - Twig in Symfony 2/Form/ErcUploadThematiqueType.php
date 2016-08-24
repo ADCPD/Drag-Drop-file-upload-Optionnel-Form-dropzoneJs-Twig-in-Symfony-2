580597c190167a3f0144dbf4254ff8e71cbf9fdc
Amir Dhaouadi <dhaouadi.amir@gmail.com>
2016-08-24 17:45:04 +0200

    mise à jour contenu

--- Drag & Drop file upload Optionnel Form - dropzone - Twig in Symfony 2/Form/ErcUploadThematiqueType.php
+++ Drag & Drop file upload Optionnel Form - dropzone - Twig in Symfony 2/Form/ErcUploadThematiqueType.php
@@ -13,36 +13,46 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
  */
 class ErcUploadThematiqueType extends AbstractType
 {
-    /**
-     * Cette methode permet de creer un input file dynamiquement 
-     * pour chaque instance de la class ERC
-     * 
+     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        /** @var Erc $erc */
-        foreach ($options["ercs"] as $index => $erc) {
-            $key = sprintf("file-%s", $erc->getId());
+
+        /** @var Formation $formation */
+        foreach ($options["formations"] as $index => $formation) {
+            $key = sprintf("file-%s", $formation->getId());
             $builder->add($key, 'file', array(
-                'label' => $erc->getLibelle(),
+                'label' => $formation->getLibelle(),
                 'required' => false,
                 'attr' => array(
                     'class' => "dropzone ",
-                 )
+                )
             ));
         }
+        $builder->add('Deposer', 'submit', array(
+            'attr' => array(
+                'class' => 'btn btn-success btn-block  pull-right '
+            )
+        ));
+
 
     }
 
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'ercs' => array()
+            'formations' => array()
         ));
     }
 
+    /**
+     * @return string
+     */
     public function getName()
     {
         return 'reglementaires_erc_bundle_erc_upload_thematique_type';
