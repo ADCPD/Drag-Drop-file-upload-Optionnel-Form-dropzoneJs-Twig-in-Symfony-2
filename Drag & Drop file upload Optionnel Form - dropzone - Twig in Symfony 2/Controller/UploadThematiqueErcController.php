7fd4e1fea37815d4157fee916fe36bed6df34e35
Amir Dhaouadi <dhaouadi.amir@gmail.com>
2016-08-25 12:24:30 +0200

    mise à jour de CreateAction method
    Modification du nom 
    Vérification du contenu  
    Transfert des données

--- Drag & Drop file upload Optionnel Form - dropzone - Twig in Symfony 2/Controller/UploadThematiqueErcController.php
+++ Drag & Drop file upload Optionnel Form - dropzone - Twig in Symfony 2/Controller/UploadThematiqueErcController.php
@@ -30,15 +30,13 @@ class UploadThematiqueErcController extends Controller
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function createAction(Request $request)
+       public function createAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
         $formations = $em->getRepository('ReglementairesErcBundle:Formation')->findAll();
-//        $form = $this->createForm(new ErcUploadThematiqueType(), array(), array('ercs' => $ercs));
         $form = $this->createForm(new ErcUploadThematiqueType(), array(), array('formations' => $formations));
 
         $request = $this->getRequest();
-
         $form->handleRequest($request);
 
         if ('POST' === $request->getMethod()) {
@@ -48,34 +46,43 @@ class UploadThematiqueErcController extends Controller
                 foreach ($formations as $index => $formation) {
 
                     $key = sprintf("file-%s", $formation->getId());
-
                     $file = $form->get($key)->getData();
-                    $name = $file->getClientOriginalName();
-                    $dir = __DIR__ . '/../../../../web/uploads/imports_csv/' . $formation->getLibelle() . '/';
-                    $file->move($dir, $name);
+
+//                    $oreginalName = $file->getClientOriginalName();
+
+                    $date = new \DateTime('now');
+                    $name = (string)$formation->getId() . "_" . $formation->getLibelle() . "_" . $date->format("Y-m-d").".csv";
+
+                    if ($file !== null) {
+                        $type = $file->getClientOriginalExtension();
+                        /** Si $type  est en format CSV l'upload se fait correctement sinon message d'erreur*/
+                        if ($type === "csv") {
+                            $dir = __DIR__ . '/../../../../web/uploads/imports_csv/thematique_formation/';
+                            $file->move($dir, $name);
+                            $this->get("session")->getFlashBag()->add(FlashMessageConsts::MESSAGE_SUCCESS, "Vos données ont été correctement enregistrées.");
+                        } else {
+//
+                            $this->get("session")->getFlashBag()->add(FlashMessageConsts::MESSAGE_WARNING, "Le fichier  n'a pas la format *.CSV");
+
+                        }
+                    }
+
 
 //                    echo "<br> Key $key  <pre>";
 //                    echo "<br> File $file  <pre>";
+//                    echo "<br> Type $type  <pre>";
 //                    echo "<br> Name $name <pre>";
 //                    echo "<br> DIR_ $dir  <pre>";
-//                    Debug::dump($file);
+////                    Debug::dump($file);
 //                    echo ' </pre>';
 //                    die();
-
-                    $this->get("session")->getFlashBag()->add(FlashMessageConsts::MESSAGE_SUCCESS, "Vos données ont été correctement enregistrées.");
-                    return $this->render('ReglementairesErcBundle:UploadThematiqueErc:new.html.twig', array(
-                        'formations' => $formations,
-                        'form' => $form->createView(),
-                    ));
                 }
+                return $this->redirect($this->generateUrl('reglementaires_thematique_import_new'));
 
             }
 
-
-        } else {
-
             $this->get("session")->getFlashBag()->add(FlashMessageConsts::MESSAGE_ERROR, "Vos données n'ont pas été correctement enregistrées.");
-            return $this->render('ReglementairesErcBundle:UploadThematiqueErc:new.html.twig', array(
+            return $this->render('ReglementairesErcBundle:UploadThematique:new.html.twig', array(
                 'formations' => $formations,
                 'form' => $form->createView(),
             ));
